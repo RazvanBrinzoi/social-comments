563b11ca78f6e31ad168679395847d3568b20655
RazvanB <45710607+RazvanBrinzoi@users.noreply.github.com>
2025-09-04 12:24:44 +0300

    Creating the components and adding the styling

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import HashtagList from "./HashtagList";
+import HeaderContainer from "./HeaderContainer";
+
+function App() {
+  return (
+    <div className="app">
+      <Footer />
+      <HeaderContainer />
+      <HashtagList />
+    </div>
+  );
+}
+
+export default App;
